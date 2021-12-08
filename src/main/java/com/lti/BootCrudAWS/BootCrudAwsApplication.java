8c4b8799b2e30f0071ed33437c6d3afbdc8eef82
SBiswajeet <sbiswajeet2345@gmail.com>
2021-12-08 12:05:48 +0530

    my project

---
+++ src/main/java/com/lti/BootCrudAWS/BootCrudAwsApplication.java
@@ -0,0 +1,13 @@
+package com.lti.BootCrudAWS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootCrudAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootCrudAwsApplication.class, args);
+	}
+
+}
